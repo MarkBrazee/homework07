6ab4d93c6ccf4e9553e23fe02430df560fbedbce
Mark Brazee <m.brazee@comcast.net>
2020-10-16 19:48:55 -0600

    working base file

--- index.js
+++ index.js
@@ -10,19 +10,23 @@
 // GIVEN a command-line application that accepts user input
 
 // WHEN I am prompted for information about my application repository
-
 // THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
+
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
 // WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 
@@ -40,7 +44,8 @@
 // -------------------------------
 
 const inquirer = require("inquirer")
-const fs = require("fs")
+const fs = require("fs");
+
 
 
 // array of questions for user
@@ -53,54 +58,61 @@ const fs = require("fs")
     // Contribution guidelines
     // Test instructions
 
+
+// Array of questions
 const questions = [ {
     
     type: "input",
     message: "What is your GitHub username",
-    ident: "ident",
-    
+    name: "ident",
+    },
+{
     type: "input",
     message: "What is your name?",
     name: "name",
-
+    },
+{    
     type: "input",
     message: "What is your email address",
-    email: "email",
-
+    name: "email",
+    },
+{
     type: "input",
     message: "Enter Project Name",
-    projName: "projName",
-
+    name: "projName",
+    },
+{
     type: "input",
     message: "Description of the project",
-    descript: "descript",
-
+    name: "descript",
+    },
+{
     type: "input",
     message: "Installation instructions",
-    install: "install",
-
+    name: "install",
+    },
+{
     type: "input",
     message: "Contribution instructions",
-    cont: "cont",
-
+    name: "cont",
+    },
+{
     type: "input",
     message: "Test instructions",
-    test: "test",
+    name: "test",
 
-}
-    
-];
+}];
 
 // function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFileSync(fileName, data)
-// }   
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
+}   
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions)
     .then(({
-        ident, name, email, projName, descript, install, cont, test,
+            ident, name, email, projName, descript, install, cont, test,
     }) => {
         const markdown = `
 ## GitHub username: ${ident},
