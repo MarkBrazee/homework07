2993ecb3f028e9827cd705d8d78bd050bef41102
Mark Brazee <m.brazee@comcast.net>
2020-10-16 20:56:47 -0600

    first ReadMe file

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+
+# Project Name: Good ReadMe Generator
+
+### Description of the Project: The Good ReadMe Generator is a Node JS file to be used by the developer as they create code or a project and then deposit it in a repository. The file prompts the developer to enter information such as, but not limited to, the project name, description of the project, installation and usage instructions about their code or project. The Generator is a tool to help the developer create a thorough and informative ReadMe file based on best practices.,
+
+### Installation instructions: Install the Good ReadMe Generator index.js file into your project folder along with the associated package.json file. Next, generate an empty npm project by typing in npm init -y at the project folder command line. Then load the Inquirer node dependency modules into your project folder by typing in npm install inquirer –save. You are now ready to use the Good ReadMe Generator.,
+
+### Usage instructions: To use the Good ReadMe Generator, open the index.js file within a program for writing code, such as VS Code, navigate to the project folder and type in node index.js in the command prompt. A question will appear in the command prompt asking for information about the project. Answer the question thoroughly and select the ‘enter’ key and another question will appear. Continue to answer all of the questions. Once, all of the questions are answered, a ReadMe file will be created and placed in the project folder.,
+
+### Contribution instructions: Please contribute to enhance the functionality of this code.,
+
+### Test instructions: Testing pending.,
+
+### Questions? Contact Mark Brazee at: m.brazee@comcast.net, or at github.com/MarkBrazee
+
--- index.js
+++ index.js
@@ -18,17 +18,17 @@
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+    // WHEN I choose a license for my application from a list of options
+    // THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+    // WHEN I enter my GitHub username
+    // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 
 // WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+    // WHEN I click on the links in the Table of Contents
+    // THEN I am taken to the corresponding section of the README
 
 // At a minimum, your project README needs a title and a short description explaining the what, why, and how. What was your motivation? Why did you build this project? (Note: The answer is not "Because it was a homework assignment.") What problem does it solve? What did you learn? What makes your project stand out? If your project has a lot of features, consider adding a heading called "Features" and listing them here.
 
@@ -43,6 +43,7 @@
 
 // -------------------------------
 
+// dependencies
 const inquirer = require("inquirer")
 const fs = require("fs");
 
@@ -93,6 +94,11 @@ const questions = [ {
     },
 {
     type: "input",
+    message: "Usage instructions",
+    name: "usage",
+    },
+{
+    type: "input",
     message: "Contribution instructions",
     name: "cont",
     },
@@ -112,24 +118,22 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
     .then(({
-            ident, name, email, projName, descript, install, cont, test,
+            ident, name, email, projName, descript, install, usage, cont, test,
     }) => {
         const markdown = `
-## GitHub username: ${ident},
-
-## Name: ${name},
-
-## Email: ${email},
-
-# Project Name: ${projName},
+# Project Name: ${projName}
 
 ### Description of the Project: ${descript},
 
 ### Installation instructions: ${install},
 
+### Usage instructions: ${usage},
+
 ### Contribution instructions: ${cont},
 
-### Test instructions: ${test}
+### Test instructions: ${test},
+
+### Questions? Contact ${name} at: ${email}, or at github.com/${ident}
 
 `
         writeToFile("ReadMe.md", markdown)
